92279c71509d7c9bdd5bcae97180aa149339ceb9
11111Asif <asif040502@gmail.com>
2025-07-07 21:04:11 -0700

    added to banner section

--- src/component/Banner/Banner.jsx
+++ src/component/Banner/Banner.jsx
@@ -12,18 +12,32 @@ const Banner = () => {
                     <img
                         src={photo}
                         className=" rounded-lg lg:w-[618px] lg:h-[581px] w-[322px] h-[484px] " />
-                    <div>
+                    <div className='lg:text-start text-center'>
                         <h5>Hi I am</h5>
                         <h3 className='text-xl py-3'>Mahmood Fazile</h3>
-                        <h1 className="lg:text-5xl text-[#CA5900] text-3xl lg:w-[534px] font-bold">UI/UX designer</h1>
-                        <div className='flex gap-2 py-8'>
+                        <h1 className="lg:text-5xl text-[#CA5900] text-3xl lg:w-[528px] font-bold">UI/UX designer</h1>
+                        <div className='flex gap-2 py-8 justify-center lg:justify-start'>
                             <img src={frame52} alt="" />
                             <img src={frame53} alt="" />
                             <img src={frame54} alt="" />
                             <img src={frame55} alt="" />
                         </div>
-                        <button className="bg-[#E46400] w-[156px] h-[48px] rounded-md">Get Started</button>
-                        <button className="border w-[209px] ml-5 h-[48px] rounded-md">Download CV </button>
+                        <button className="bg-[#E46400] w-[85px] h-[33px] lg:w-[156px] lg:h-[48px] rounded-md">Get Started</button>
+                        <button className="border w-[150px] lg:w-[209px] ml-5 h-[33px] lg:h-[48px] rounded-md">Download CV </button>
+                        <div className='lg:w-[534px] w-[340px] lg:h-[125px] h-[99px] grid grid-cols-3 py-8 rounded-md mt-12 bg-[#FFFFFF0A]'>
+                            <div className='border-r px-1 lg:px-5 text-start'>
+                                <h4 className='text-[#FD6F00] font-bold'>5+</h4>
+                                <p>Experiences</p>
+                            </div>
+                            <div className='border-r px-1 lg:px-5 text-start'>
+                                <h4 className='text-[#FD6F00] font-bold'>20+</h4>
+                                <p>Project done</p>
+                            </div>
+                            <div className='lg:px-5 px-1 text-start'>
+                                <h4 className='text-[#FD6F00] font-bold'>80+</h4>
+                                <p>Happy Clients</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
